d68165a114e014502dc28ac899441dd2192d485c
Dalek Bob <support@caffeinatedprojects.com>
2022-08-05 03:01:16 +0100

    Update create_docker.sh

--- scripts/create_docker.sh
+++ scripts/create_docker.sh
@@ -7,7 +7,7 @@ sudo rm -rf /home/$acc_user/logs/create_docker_processing
 sudo rm -rf /home/$acc_user/logs/create_docker_done
 
 touch /home/$acc_user/logs/create_docker_processing
-exec &> /home/$acc_user/logs/create_docker_processing
+exec 1> >(tee -a "/home/$acc_user/logs/create_docker_processing")
 
 echo '--------- Script Start ---------' 
 
@@ -17,24 +17,7 @@ if test -f "$FILE"; then
     sudo chown $acc_user:$acc_user $FILE
 else
     echo "Environment file not found"
-fi
-
-# Confirm .env file exists
-# if [ -f .env ]; then
-
-#     # Create tmp clone
-#     cat .env > .env.tmp;
-
-#     # Subtitutions + fixes to .env.tmp2
-#     cat .env.tmp | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" > .env.tmp2
-
-#     # Set the vars
-#     set -a; source .env.tmp2; set +a
-
-#     # Remove tmp files
-#     rm .env.tmp .env.tmp2
-
-# fi 
+fi 
  
 cd /home/$acc_user/
 sudo make install
@@ -47,8 +30,9 @@ else
     echo "wordpress install failed"
 fi
 
-mv /home/$acc_user/logs/create_docker_processing /home/$acc_user/logs/create_docker__done
-
 echo '--------- Script END ---------'
 
-exit
+
+sleep 1
+mv /home/$acc_user/logs/create_docker_processing /home/$acc_user/logs/create_docker_done
+exit 0
