350d66b14084663e2eaf9bab81cd37081c27d7be
Dalek Bob <me@andyhughes.co.uk>
2023-09-08 12:05:03 +0100

    Create create_sudo_user.sh

---
+++ automation/scripts/create_sudo_user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+ 
+## $1 : user_name
+## $2 : user_pass  
+
+acc_user=$1 
+acc_pass=$2    
+ 
+#if [ -f "/home/$acc_user" ]; then
+    sudo userdel -r $acc_user
+    sudo rm -rf /home/$acc_user/*
+#fi 
+
+sudo adduser $acc_user --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo $acc_user:$acc_pass | chpasswd  
+
+sudo usermod -aG sudo $acc_user 
+
+if getent passwd $acc_user > /dev/null 2>&1; then
+    echo "the user exists"
+else
+    echo "the user does not exist"
+fi
