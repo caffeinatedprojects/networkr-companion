b06095d504ce8198ace088412f9b56640592a5b2
kronankreative <me@andyhughes.co.uk>
2023-11-20 12:08:34 +0000

    Update sync_makefile.sh

--- automation/scripts/sync_makefile.sh
+++ automation/scripts/sync_makefile.sh
@@ -26,17 +26,17 @@ do
 	
 	echo 'Name: ' $NAME 
 
-	sudo mv $TEMPLATE_PATH/Makefile USER_PATH/Makefile
-	sudo chown $USER:$USER USER_PATH/Makefile
+	sudo mv $TEMPLATE_PATH/Makefile $USER_PATH/Makefile
+	sudo chown $USER:$USER $USER_PATH/Makefile
 
-	sudo mv $TEMPLATE_PATH/docker-compose.yml USER_PATH/docker-compose.yml
-	sudo chown $USER:$USER USER_PATH/docker-compose.yml
+	sudo mv $TEMPLATE_PATH/docker-compose.yml $USER_PATH/docker-compose.yml
+	sudo chown $USER:$USER $USER_PATH/docker-compose.yml
 
-	sudo mv $TEMPLATE_PATH/_config.yml USER_PATH/_config.yml
-	sudo chown $USER:$USER USER_PATH/_config.yml
+	sudo mv $TEMPLATE_PATH/_config.yml $USER_PATH/_config.yml
+	sudo chown $USER:$USER $USER_PATH/_config.yml
 	
-	sudo mv $TEMPLATE_PATH/conf.d/php.ini USER_PATH/conf.d/php.ini
-	sudo chown $USER:$USER USER_PATH/conf.d/php.ini 
+	sudo mv $TEMPLATE_PATH/conf.d/php.ini $USER_PATH/conf.d/php.ini
+	sudo chown $USER:$USER $USER_PATH/conf.d/php.ini 
 
 	echo '------------------------------'
 
