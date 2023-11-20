66e89e9340ad12c00f58b3611a90257fff72ab1b
kronankreative <me@andyhughes.co.uk>
2023-11-20 12:24:44 +0000

    Update sync_makefile.sh

--- automation/scripts/sync_makefile.sh
+++ automation/scripts/sync_makefile.sh
@@ -26,16 +26,16 @@ do
 	
 	echo 'Name: ' $NAME 
 
-	sudo mv $TEMPLATE_PATH/Makefile $USER_PATH/Makefile
+	sudo cp $TEMPLATE_PATH/Makefile $USER_PATH/Makefile
 	sudo chown $USER:$USER $USER_PATH/Makefile
 
-	sudo mv $TEMPLATE_PATH/docker-compose.yml $USER_PATH/docker-compose.yml
+	sudo cp $TEMPLATE_PATH/docker-compose.yml $USER_PATH/docker-compose.yml
 	sudo chown $USER:$USER $USER_PATH/docker-compose.yml
 
-	sudo mv $TEMPLATE_PATH/_config.yml $USER_PATH/_config.yml
+	sudo cp $TEMPLATE_PATH/_config.yml $USER_PATH/_config.yml
 	sudo chown $USER:$USER $USER_PATH/_config.yml
 	
-	sudo mv $TEMPLATE_PATH/conf.d/php.ini $USER_PATH/conf.d/php.ini
+	sudo cp $TEMPLATE_PATH/conf.d/php.ini $USER_PATH/conf.d/php.ini
 	sudo chown $USER:$USER $USER_PATH/conf.d/php.ini 
 
 	echo '------------------------------'
