ac12308157e365740b5b7be47c2e85c8aba51dfb
kronankreative <me@andyhughes.co.uk>
2023-11-20 12:06:17 +0000

    Backups

--- automation/scripts/daily_backups.sh
+++ automation/scripts/daily_backups.sh
@@ -1,58 +1,50 @@
-#!/bin/bash 
-
-mkdir -p ~/logs/backup
-
-processing=`date +%Y-%m-%d_log_processing`
-completed=`date +%Y-%m-%d_log_completed` 
-
-touch ~/logs/backup/$processing
-exec &> ~/logs/backup/$processing 
+#!/bin/bash  
 
 echo '----- Start Daily Back Up ----' 
 date '+%d/%m/%Y_%H:%M:%S'  
 
-OUTPUT_JSON=$(cat ~/data/users.json | jq '.backups') 
-# echo $OUTPUT_JSON
+OUTPUT_JSON=$(cat /home/networkr/data/users.json | jq '.backups') 
+echo $OUTPUT_JSON
 
-# Get the length of the list:
 LENGTH_NAMES=$(echo $OUTPUT_JSON | jq length)
 
-# An array to store all names:
-ARR_NAMES=()
-
 YEAR=$(date +%Y)
-MONTH=$(date +%m)  
+MONTH=$(date +%m) 
 
-# Iterate through to store the names:
-for((i=0; i < $LENGTH_NAMES; ++i)); do
+echo $(date +%Y)
+echo $(date +%m) 
 
-	# Extract the "name" and remove parenthesis:
+i=0
+while [ $i -ne $LENGTH_NAMES ]
+do
+
+    echo "$i"
+  
 	NAME=$(echo $OUTPUT_JSON | jq ".[$i].name" | sed s'/\"//g')
 	USER=$(echo $OUTPUT_JSON | jq ".[$i].username" | sed s'/\"//g')
 	USERID=$(echo $OUTPUT_JSON | jq ".[$i].user_id" | sed s'/\"//g')
 	SERVERID=$(echo $OUTPUT_JSON | jq ".[$i].server_id" | sed s'/\"//g')
-	PATH=/home/$USER
-	FILE='backup-'$$(date '+%Y-%m-%d')'.tar.gz'
+	THEPATH=/home/$USER
+	FILE='backup-'$(date '+%Y-%m-%d')'.tar.gz'
 	
 	echo '----------- Starting Back Up : ' $USER ' -----------'
 	
 	echo 'Name: ' $NAME
 
-	cd $PATH
-
-	sudo make backup
+	cd /home/$USER/
+	sudo make backup 
 
 	echo 'Moving to Storage: '$backup_file  
 
-	s3cmd put $PATH/data/backup/$FILE s3://caffeinated-media/pressillion/backups/USERID/$YEAR/$MONTH/SERVERID/$FILE
+	s3cmd put $THEPATH/data/backup/$FILE s3://caffeinated-media/pressillion/backups/USERID/$YEAR/$MONTH/SERVERID/$FILE
    
-	rm  -r -f $PATH/backup/* 
+	rm  -r -f $THEPATH/backup/* 
 
 	echo '------------------------------'
+
+	i=$(($i+1))
 
 done 
 
 echo '----- End Daily Backs Up ----'
 date '+%d/%m/%Y_%H:%M:%S'
-
-mv ~/logs/backup/$processing ~/logs/backup/$completed 
---
+++ automation/scripts/sync_makefile.sh
@@ -0,0 +1,48 @@
+#!/bin/bash  
+
+echo '----- Start Sync Makefile ----' 
+date '+%d/%m/%Y_%H:%M:%S'  
+
+OUTPUT_JSON=$(cat /home/networkr/data/users.json | jq '.backups')
+
+TEMPLATE_PATH="/home/networkr/networkr-companion/template"
+
+echo $OUTPUT_JSON
+
+LENGTH_NAMES=$(echo $OUTPUT_JSON | jq length)   
+
+i=0
+
+while [ $i -ne $LENGTH_NAMES ]
+do
+
+    echo "$i"
+  
+	NAME=$(echo $OUTPUT_JSON | jq ".[$i].name" | sed s'/\"//g')
+	USER=$(echo $OUTPUT_JSON | jq ".[$i].username" | sed s'/\"//g') 
+	USER_PATH=/home/$USER 
+	
+	echo '----------- Starting Back Up : ' $USER ' -----------'
+	
+	echo 'Name: ' $NAME 
+
+	sudo mv $TEMPLATE_PATH/Makefile USER_PATH/Makefile
+	sudo chown $USER:$USER USER_PATH/Makefile
+
+	sudo mv $TEMPLATE_PATH/docker-compose.yml USER_PATH/docker-compose.yml
+	sudo chown $USER:$USER USER_PATH/docker-compose.yml
+
+	sudo mv $TEMPLATE_PATH/_config.yml USER_PATH/_config.yml
+	sudo chown $USER:$USER USER_PATH/_config.yml
+	
+	sudo mv $TEMPLATE_PATH/conf.d/php.ini USER_PATH/conf.d/php.ini
+	sudo chown $USER:$USER USER_PATH/conf.d/php.ini 
+
+	echo '------------------------------'
+
+	i=$(($i+1))
+
+done 
+
+echo '----- End Sync Makefile ----' 
+date '+%d/%m/%Y_%H:%M:%S'
