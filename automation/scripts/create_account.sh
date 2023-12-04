13e924ca9e715d207d723618c9f2e367162d5a5c
kronankreative <me@andyhughes.co.uk>
2023-12-04 00:07:48 +0000

    Update create_account.sh

--- automation/scripts/create_account.sh
+++ automation/scripts/create_account.sh
@@ -18,11 +18,10 @@ echo $acc_user:$acc_pass | chpasswd
 
 echo '***** Created Hosting *******' 
 echo 'created folders'  
-sudo cp -a /home/networkr/networkr-companion/template/. /home/$acc_user/
 
-sudo mkdir -p /home/$acc_user/.ssh 
-sudo rsync --archive --chown=$acc_user:$acc_user /home/$acc_user/.ssh/authorized_keys
-sudo chown -R networkr:networkr /home/$acc_user/.ssh
+sudo cp -a /home/networkr/networkr-companion/template/. /home/$acc_user/
+sudo rsync --archive --chown=$acc_user:$acc_user /home/networkr/.ssh /home/$acc_user
+
 sudo chown -R $acc_user:$acc_user /home/$acc_user/data/* 
 sudo rm /home/$acc_user/.env-example 
 
